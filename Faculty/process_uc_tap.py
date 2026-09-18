78781929c24b4a982b6dcd557ec6da6d4d0d5699
Mcray3000 <mccay.ruddick@gmail.com>
2026-09-18 11:52:16 -0700

    feat: process and clean UC faculty xcel file. Created simple EDA to hold department Cleaning

---
+++ Faculty/process_uc_tap.py
@@ -0,0 +1,43 @@
+# Lossless data processing for the UC Faculty Dataset provided by a CPRA request to the UC System
+
+import polars as pl
+
+def main():
+    print("Scanning xlsx files...")
+    
+    
+    df_dict = pl.read_excel("data/raw/**/*.xlsx",sheet_id=0)
+    df = pl.concat(df_dict.values())
+    df = df.rename(str.lower)
+    
+    df = df.with_columns(
+        pl.selectors.string().str.to_lowercase()
+    ).with_columns(
+        # Since we just have year I figure datetime obj isnt worth it
+        pl.col('year').cast(pl.Int16),
+        
+        # Change ints
+        pl.col('base_pay').cast(pl.Int32),
+        pl.col('overtime_pay').cast(pl.Int32),
+        pl.col('other_pay').cast(pl.Int32),
+        pl.col('gross_pay').cast(pl.Int32),
+
+        
+        # Cast to Categorical
+        pl.col('campus').cast(pl.Categorical),
+        pl.col('department').cast(pl.Categorical),
+    )
+    
+    
+    df.write_parquet("data/uc-faculty.parquet")
+    
+    print(f"Processed {df.height:,} records.")
+    
+    print(df.head(5))
+    
+    print(df.describe())
+    
+    print(df.schema)
+
+if __name__ == "__main__":
+    main()
